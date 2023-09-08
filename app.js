2d1a11c1e0eeeb3a3676bf96a49c8f0175309ae9
Natalia Konovalova <nutus@yandex.ru>
2023-09-09 00:34:44 +0300

    app.js_cors

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const router = require('./routes/index');
-const limitReq = require('./middlewares/rateLimiter');
 
 const app = express();
 app.disable('x-powered-by');
@@ -18,16 +17,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 
 const { PORT = 3000 } = process.env;
-let addressDB;
-
-if (process.env.NODE_ENV === 'production') {
-  addressDB = process.env.DATABASE_URL;
-} else {
-  addressDB = 'mongodb://127.0.0.1:27017/bitfilmsdb';
-}
-
-mongoose.connect(addressDB).then(() => {
-  console.log('connected to MongoDB');
+mongoose.connect('mongodb://127.0.0.1:27017/moviedb').then(() => {
 });
 
 app.use(cors({
@@ -35,7 +25,6 @@ app.use(cors({
   credentials: true,
 }));
 app.use(requestLogger); // подключаем логгер запросов
-app.use(limitReq);
 app.use(router);
 app.use(errorLogger); // подключаем логгер ошибок
 app.use(errorHandler);
